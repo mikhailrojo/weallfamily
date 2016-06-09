2a8200127e754404fafe1b016fccc807644e4d42
misha <misha.stepanov@mail.ru>
2016-06-09 20:02:07 +0300

    при распаковке в локальную базу добавляются пользователи

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,4 +1,5 @@
 import {Mongo} from 'meteor/mongo';
+let db = 'mongodb://meteorx:meteorx@ds011374.mlab.com:11374/meteorx';
 export const Users = new Mongo.Collection('newUsers');
 
 if(Meteor.isServer){
